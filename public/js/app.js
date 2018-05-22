6b4d4cf01db493464a368c329a7e1d2ad15958af
User <bo4arovpavel1989@mail.ru>
2018-05-22 09:34:32 +0300

    need to sort out

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
-console.log(React)
+import { BrowserRouter } from 'react-router-dom';
+
+alert(1)
+
+const App = ()=>{
+	return (
+	<div>
+		<Header/>
+		
+		<List/>
+	</div>	
+	);	
+}
+
+ReactDom.render(
+	<App/>,
+	document.getElementById('root')
+)
