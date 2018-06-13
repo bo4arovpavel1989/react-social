de19eba5940063ff5335b8350286fc2e2af844e6
User <bo4arovpavel1989@mail.ru>
2018-06-13 09:44:56 +0300

    finish soon register

--- src/components/common/Register.js
+++ src/components/common/Register.js
@@ -21,8 +21,8 @@ class Register extends React.Component {
 		
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-		this.checkPassword = this.checkPassword.bind(this);
 		this.checkValidity = this.checkValidity.bind(this);
+		this.checkPasswd = this.checkPasswd.bind(this);
 	}
 		
 	handleChange(e) {
@@ -31,41 +31,55 @@ class Register extends React.Component {
 			allFieldsUsed;
 				
 		state[fieldType] = e.target.value.toString();
-		
-		
+				
 		this.setState(state,()=>{
 
 			allFieldsUsed = this.state.loginValid && 
-			this.state.emailValid	&& 
-			this.state.login !== '' && 
-			this.state.passwd1 !== '' && 
-			this.state.email !== '' && 
-			this.state.passwdCorrect;
+				this.state.emailValid	&& 
+				this.state.login !== '' && 
+				this.state.passwd1 !== '' && 
+				this.state.email !== '' && 
+				this.state.passwdCorrect;
+			
+			console.log(allFieldsUsed)
+			console.log(this.state)
 			
 			this.setState({allFieldsUsed})
 			
 		});
 	}
 	
-	checkValidity(inp){
+	checkValidity(inp, cb){
 		let val = document.getElementById(inp).value;
 		
-		fetch(`${API_URL}/checkvalidity`,{
-				method:'POST',
-				mode:'cors',
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body:JSON.stringify({val,inp})
-			})
-			.then(handleResponse)
-			.then((rep)=>{
-					this.setState(rep)
-			})
-			.catch((error) => {
-				console.log(error)
-			});
+		if(inp === 'login' || inp === 'email')
+			fetch(`${API_URL}/checkvalidity`,{
+					method:'POST',
+					mode:'cors',
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					},
+					body:JSON.stringify({val,inp})
+				})
+				.then(handleResponse)
+				.then((rep)=>{
+						this.setState(rep)
+						cb();
+				})
+				.catch((error) => {
+					console.log(error)
+					cb();
+				});
+		else 
+			cb();
+	}
+	
+	checkPasswd(){
+		let passwd1 = document.getElementById('passwd1').value,
+			passwd2 = document.getElementById('passwd2').value;
+			
+		this.setState({passwdCorrect:(passwd1 === passwd2)})	
 	}
 	
 	handleSubmit(e){
@@ -95,9 +109,6 @@ class Register extends React.Component {
 			});
 	}
 	
-	checkPassword(){
-		return (this.state.passwd1 === this.state.passwd2);
-	}
 	
 	render(){
 		
@@ -105,7 +116,8 @@ class Register extends React.Component {
 		<div>
 			<form className="form-horizontal" onSubmit={this.handleSubmit}>
 				<div className="form-group">
-					<label htmlFor="inputLogin" className={"col-sm-2 control-label" + (this.state.loginValid ? '' : 'invalid')}>Логин</label>
+					<label htmlFor="inputLogin" className={"col-sm-2 control-label" + (this.state.loginValid ? '' : ' invalid')}>Логин</label>
+					<span className={'validMessage ' + (this.state.loginValid ? 'hidden' : '')}>Логин уже занят!</span>
 					<div className="col-sm-10">
 						<div className="input-group">
 							<input type="login" onChange={(this.handleChange)} onKeyUp={()=>this.checkValidity('login')} className={"form-control " + (this.state.loginValid ? '' : 'invalid')} id="login" placeholder="Login" />
@@ -113,19 +125,21 @@ class Register extends React.Component {
 					</div>
 				</div>
 				<div className="form-group">
-					<label htmlFor="inputPassword" className="col-sm-2 control-label">Пароль</label>
+					<label htmlFor="inputPassword" className={"col-sm-2 control-label" + (this.state.passwdCorrect ? '' : ' invalid')}>Пароль</label>
+					<span className={'validMessage ' + (this.state.passwdCorrect ? 'hidden' : '')}>Пароли должны совпадать!</span>
 					<div className="col-sm-10">
-						<input type="password"  value={this.state.passwd} onChange={this.handleChange} className="form-control" id="passwd1" placeholder="Password"/>
+						<input type="password"  value={this.state.passwd} onChange={this.handleChange}  onKeyUp={this.checkPasswd} className={"form-control " + (this.state.passwdCorrect ? '' : 'invalid')} id="passwd1" placeholder="Password"/>
 					</div>
 				</div>
 				<div className="form-group">
 					<label htmlFor="inputPassword" className="col-sm-2 control-label">Повторите пароль</label>
 					<div className="col-sm-10">
-						<input type="password"  value={this.state.passwd} onChange={this.handleChange} className="form-control" id="passwd2" placeholder="Password"/>
+						<input type="password"  value={this.state.passwd} onChange={this.handleChange}  onKeyUp={this.checkPasswd} className="form-control" id="passwd2" placeholder="Password"/>
 					</div>
 				</div>
 				<div className="form-group">
-					<label htmlFor="inputLogin" className={"col-sm-2 control-label"+ (this.state.emailValid ? '' : 'invalid')}>E-Mail</label>
+					<label htmlFor="inputLogin" className={"col-sm-2 control-label"+ (this.state.emailValid ? '' : ' invalid')}>E-Mail</label>
+					<span className={'validMessage ' + (this.state.emailValid ? 'hidden' : '')}>Пользователь с данным E-Mail уже зарегистрирован!</span>
 					<div className="col-sm-10">
 						<div className="input-group">
 							<input type="email" onChange={this.handleChange}  onKeyUp={()=>this.checkValidity('email')} className={"form-control " + (this.state.emailValid ? '' : 'invalid')} id="email" placeholder="E-Mail" />
