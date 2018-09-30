38412ba3150c5e7e53b1f6d85dac8d46d3b72ed9
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2018-09-30 17:48:52 +0300

    refactored cash middleware

--- api/customfunctions.js
+++ api/customfunctions.js
@@ -4,11 +4,13 @@ const secret = require('./credentials');
 const async = require('async');
 const fs = require('fs-extra');
 const easyimg = require('easyimage');
+const cache = require('memory-cache');
+let memCache = new cache.Cache();
 
-module.exports.authService = {
+const authService = {
 	login:function(cred){
 		return new Promise((resolve, reject)=>{
-			
+
 			db.findOne('User',{loginUpperCase:cred.login.toUpperCase(),passwd:cred.passwd})
 				.then(res=>{
 					if(!res) resolve({auth:false,res:null});
@@ -17,51 +19,87 @@ module.exports.authService = {
 				.catch(err=>{
 					reject(err);
 				})
-				
+
 		})
-		
+
 	},
 	checkToken:function(data){
 		return new Promise((resolve, reject)=>{
-			
+
 			db.findOne('Session', {login:data.login,token:data.token})
 				.then(rep=>{
 					if(rep) resolve(true);
 					else resolve (false);
 				})
 				.catch(err=>reject(err))
-				
+
 		})
 	},
 	logoff:function(data){
 		return new Promise((resolve, reject)=>{
-		
+
 			db.del('Session',data)
 				.then(rep=>resolve(rep))
 				.catch(err=>reject(err))
-			
-		});	
+
+		});
 	}
 }
+
+module.exports.authService = authService;
 
 function setToken(res){
 	return jwt.sign({login:res.login, pass:res.passwd, email:res.email, date:new Date()}, secret.secret);
 }
 
+
+module.exports.cacheMiddleware = function (duration, req, res, next) {
+	let key =  '__express__' + req.originalUrl || req.url
+	let cacheContent = memCache.get(key);
+
+	if(cacheContent){
+		res.json( cacheContent );
+	} else {
+		res.sendJSON= res.json;
+
+		res.json = (body) => {
+			memCache.put(key,body,duration*1000);
+			res.sendJSON(body)
+		}
+
+		next();
+	}
+}
+
+module.exports.checkAccessMiddleware = function (req) {
+	let cred;
+
+	if(req.headers.id)
+		cred = req.headers;
+	else
+		cred = req.body;
+
+	return new Promise((resolve, reject) => {
+		authService.checkToken(cred)
+				.then( rep => resolve(rep) )
+				.catch( err => reject(err) )
+	})
+}
+
 module.exports.saveAvatar = function(files,filename,dst,dst2){
 	return new Promise((resolve, reject)=>{
-		
+
 		async.series([
 			(cb)=>{
 				makeResize(files,dst,200,200,(err,rep)=>{
 					if(err) cb(new Error(err))
-					else cb();	
+					else cb();
 				})
 			},
 			(cb)=>{
 				makeResize(files,dst2,50,50,(err,rep)=>{
 					if(err) cb(new Error(err))
-					else cb();	
+					else cb();
 				})
 			},
 			(cb)=>{
@@ -86,18 +124,18 @@ module.exports.saveAvatar = function(files,filename,dst,dst2){
 
 function makeResize(files,dst,w,h,cb){
 	easyimg.resize({src: files.file.path, dst: dst, width:w, h}, function(err, stdout, stderr) {
-			if (err) 
+			if (err)
 				fs.unlink(files.file.path, (err)=>cb('Error loading avatar', false));
 		}).then(
 				(img) => cb(null, true),
 				(err) => fs.unlink(files.file.path, err=>cb('Error resizing, source delete', false))
-		); 	
+		);
 }
 
 module.exports.handleLike = function(rep, _id, liker){
-	if(!rep) 
+	if(!rep)
 		db.update('Wall', {_id}, {$push: {likers: liker}, $inc:{like:1}});
-	else 
+	else
 		db.update('Wall', {_id}, {$pull: {likers: {$in: [liker]}}, $inc:{like:-1}});
 }
 
@@ -114,11 +152,11 @@ module.exports.markMessagesSeen = function(rep){
 };
 
 module.exports.checkBan = function(me, person){
-	
+
 	return new Promise((resolve, reject) => {
 		db.findOne('BlackList', {person, list: {$in: [me]}}) //check if i was banned by person
 			.then(rep => resolve(rep !== null) ) //returns true if user is banned
 			.catch(err => reject(err))
 	});
-	
+
 }
--- api/middlewarefunctions.js
+++ api/middlewarefunctions.js
@@ -1,65 +1,32 @@
 const authService = require('./customfunctions').authService;
+const cacheMiddleware = require('./customfunctions').cacheMiddleware;
+const checkAccessMiddleware = require('./customfunctions').checkAccessMiddleware;
 const formidable = require('formidable');
-const cache = require('memory-cache');
-
-let memCache = new cache.Cache();
-
-let cacheMiddleware = (duration, req, res, next) => {
-	let key =  '__express__' + req.originalUrl || req.url
-	let cacheContent = memCache.get(key);
-
-	if(cacheContent){
-		res.json( cacheContent );
-	} else {
-		res.sendJSON= res.json;
-
-		res.json = (body) => {
-			memCache.put(key,body,duration*1000);
-			res.sendJSON(body)
-		}
-
-		next();
-	}
-}
 
 module.exports.noMiddleware = function(req, res, next){
 	next();
 };
 
 module.exports.checkAccess = function(req, res, next){
-	let cred;
-
-	if(req.headers.id)
-		cred = req.headers;
-	else
-		cred = req.body;
-
-	authService.checkToken(cred)
-			.then(rep=>{
-				if(rep)
-					next();
-				else
-					res.json({forbidden:true});
-			})
-			.catch(err=>res.status(500).json({err:err}))
+	checkAccessMiddleware(req)
+		.then(rep=>{
+			if(rep)
+				next();
+			else
+				res.json( {forbidden:true} );
+		})
+		.catch( err => res.status(500).json({err}) )
 };
 
 module.exports.checkAccessAndCash = function(req, res, next){
-	let cred;
-
-	if(req.headers.id)
-		cred = req.headers;
-	else
-		cred = req.body;
-
-	authService.checkToken(cred)
-			.then(rep=>{
-				if(rep)
-					cacheMiddleware(5, req, res, next);
-				else
-					res.json({forbidden:true});
-			})
-			.catch(err=>res.status(500).json({err:err}))
+	checkAccessMiddleware(req)
+		.then(rep=>{
+			if(rep)
+				cacheMiddleware(10, req, res, next);
+			else
+				res.json( {forbidden:true} );
+		})
+		.catch( err => res.status(500).json({err}) )
 };
 
 module.exports.checkFileAccess = function(req, res, next){
