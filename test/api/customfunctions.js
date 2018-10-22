d85135b36b5d376734997b3786bc514e7f2c0d27
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2018-10-22 20:03:57 +0300

    made some tests. But it make no sense

--- test/api/customfunctions.js
+++ test/api/customfunctions.js
@@ -1,6 +1,9 @@
-const {describe, it} = require('mocha'),
+const sinon = require('sinon'),
+  {describe, it} = require('mocha'),
   {expect} = require('chai'),
-  customFunctions = require('../../api/customfunctions');
+  easyimg = require('easyimage'),
+  customFunctions = require('../../api/customfunctions'),
+  db = require('../../api/dbqueries')
 
 describe('setToken', ()=>{
   it('returns jwt based on response object data', ()=>{
@@ -8,5 +11,48 @@ describe('setToken', ()=>{
     const {setToken} = customFunctions;
     const result = setToken(response);
     expect(result).to.be.a('string');
+  });
+});
+
+describe('authService', ()=>{
+
+  beforeEach(function () {
+      this.sandbox = sinon.createSandbox()
+  })
+
+  afterEach(function () {
+      this.sandbox.restore()
+  })
+
+  const {authService} = customFunctions;
+
+  it('should return promise with token data if correct', function * () {
+    const cred = {login:'login', passwd:'pass'};
+    const query = {loginUpperCase:cred.login.toUpperCase(), passwd:cred.pass}
+    const dbStub = this.sandbox.stub(db, 'findOne', (q, cb) => {
+      cb({auth:true, id:'id', login:'login', token:'token'});
+    });
+
+    const result = yield authService.login(cred);
+
+    expect(dbStub).to().be().calledWith(query);
+    expect(result).to().eql({auth:true, id:'id', login:'login', token:'token'})
+  })
+});
+
+describe('make resize', ()=>{
+  const {makeResize} = customFunctions;
+
+  beforeEach( function() {
+       this.easyImgStub = sinon.stub(easyimg, 'resize');
+   });
+
+   afterEach( function() {;
+       this.easyImgStub.restore();
+   });
+
+  it('should callbak true when resized', function * (){
+    this.easyImgStub.returns(true);
+    expect(makeResize).to().eql(true);
   });
 });
